fcccfab963334345c10d471c61aa798576f9be70
ChenKaiKe <1448657054@qq.com>
2022-07-11 23:19:55 +0800

    feat: 初始化项目

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: Kaike Chen
+ * @Date: 2022-07-11 22:37:18
+ * @LastEditors: Kaike Chen
+ * @LastEditTime: 2022-07-11 22:39:31
+ * @Description: vuepress的配置文件，详情请查看https://v2.vuepress.vuejs.org/zh/guide/configuration.html#%E9%85%8D%E7%BD%AE%E6%96%87%E4%BB%B6
+ */
+module.exports = {
+  lang: 'zh-CN',
+  title: '你好， VuePress ！',
+  description: '这是我的第一个 VuePress 站点',
+}
+
---
+++ docs/README.md
@@ -0,0 +1,8 @@
+<!--
+ * @Author: Kaike Chen
+ * @Date: 2022-07-11 22:22:46
+ * @LastEditors: Kaike Chen
+ * @LastEditTime: 2022-07-11 22:36:06
+ * @Description: 
+-->
+# Hello VuePress
