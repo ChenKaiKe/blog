4362878998833fe7b86d4758b3fa0e1d7758bf33
ChenKaiKe <1448657054@qq.com>
2022-07-24 11:16:38 +0800

    feat: 初始化项目02

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,19 @@
+const { defaultTheme } = require('vuepress')
+
 module.exports = {
   lang: 'zh-CN',
   title: '你好， VuePress ！',
   description: '这是我的第一个 VuePress 站点',
+  base: '/blog/',
+  head: [['link', { rel: 'icon', href: '/images/hero.png' }]],
+  theme: defaultTheme({
+    // 默认主题配置
+    navbar: [
+      {
+        text: '首页',
+        link: '/',
+      },
+    ],
+  }),
 }
 
--- docs/README.md
+++ docs/README.md
@@ -1,8 +1,1 @@
-<!--
- * @Author: Kaike Chen
- * @Date: 2022-07-11 22:22:46
- * @LastEditors: Kaike Chen
- * @LastEditTime: 2022-07-11 22:36:06
- * @Description: 
--->
 # Hello VuePress
